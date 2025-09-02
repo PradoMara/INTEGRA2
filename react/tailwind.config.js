bdc6ba6a6ecb6c193428616ebfec133137084a7d
Martín Sanhueza F. <msanhueza2024@alu.uct.cl>
2025-09-02 03:27:56 -0400

    Creación de Botones, Inputs y Modales (Estilo Base)
    Creación de componentes básicos sin estilos ya que surgieron problemas con tailwind css, se tratara de mejorar lo antes posible

---
+++ react/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Todas las rutas de archivos fuente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
